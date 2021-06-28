2399e76a856cf25557b244a31b49f0ef4736cace
Yana Rozova <yana.rozova@mail.ru>
2021-06-28 23:03:29 +0300

    Открытие модального окга с формой обратной связи

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 /* slider buttons */
-let buttonPrev = document.querySelector(".button-prev");
-let buttonNext = document.querySelector(".button-next");
-let slider = document.querySelectorAll(".slider-img");
-let sliderBullets = document.querySelectorAll(".slider-bullet");
+const buttonPrev = document.querySelector(".button-prev");
+const buttonNext = document.querySelector(".button-next");
+const slider = document.querySelectorAll(".slider-img");
+const sliderBullets = document.querySelectorAll(".slider-bullet");
 let addChecked = function (index) {
 	sliderBullets[index].checked = true;
 };
 let delChecked = function (index) {
 	sliderBullets[index].checked = false;
 };
-let sliderLength = slider.length;
-var index = 0;
+const sliderLength = slider.length;
+let index = 0;
 buttonNext.addEventListener("click", function () {
 	if (index < sliderLength - 1) {
 		delChecked(index);
@@ -31,5 +31,42 @@ buttonPrev.addEventListener("click", function () {
 		delChecked(0);
 		index = sliderLength - 1;
 		addChecked(index);
+	}
+});
+/* feedback modal window */
+const buttonFeedBack = document.querySelector(".button-contacts");
+const body = document.querySelector(".page-body");
+const feedBackWindow = document.querySelector(".feed-back-window");
+const closeFeedBack = feedBackWindow.querySelector(".close-feedback");
+const feedBackForm = feedBackWindow.querySelector(".feed-back-form");
+const feedBackNameInput = feedBackWindow.querySelector(".name-input");
+const feedBackEmailInput = feedBackWindow.querySelector(".email-input");
+let isStorageSupport = true;
+let storage = "";
+try {
+	storage = localStorage.getItem("name");
+} catch (err) {
+	isStorageSupport = false;
+}
+buttonFeedBack.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	feedBackWindow.classList.add("active");
+	if(storage){
+		feedBackNameInput.value=storage;
+	}
+	feedBackNameInput.focus();
+	body.classList.add("body-blocked");
+});
+closeFeedBack.addEventListener("click", function () {
+	feedBackWindow.classList.remove("active");
+	body.classList.remove("body-blocked");
+});
+feedBackForm.addEventListener("submit", function (evt) {
+	if (!feedBackNameInput.value || !feedBackEmailInput.value) {
+		evt.preventDefault();
+	} else {
+		if (isStorageSupport) {
+			localStorage.setItem("name", feedBackNameInput.value);
+		}
 	}
 });
