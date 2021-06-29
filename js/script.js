11c0ade34c1bde20d9f5065f2166fb110a1c80e7
Yana Rozova <yana.rozova@mail.ru>
2021-06-29 14:07:14 +0300

    Добавление события закрытия окна по нажатию ESC

--- js/script.js
+++ js/script.js
@@ -3,35 +3,28 @@ const buttonPrev = document.querySelector(".button-prev");
 const buttonNext = document.querySelector(".button-next");
 const slider = document.querySelectorAll(".slider-img");
 const sliderBullets = document.querySelectorAll(".slider-bullet");
-let addChecked = function (index) {
-	sliderBullets[index].checked = true;
-};
-let delChecked = function (index) {
-	sliderBullets[index].checked = false;
-};
-const sliderLength = slider.length;
 let index = 0;
 buttonNext.addEventListener("click", function () {
-	if (index < sliderLength - 1) {
-		delChecked(index);
-		index++;
-		addChecked(index);
-	} else {
-		delChecked(sliderLength - 1);
+	if (index === slider.length - 1) {
+		sliderBullets[index].checked = false;
+		sliderBullets[0].checked = true;
 		index = 0;
-		addChecked(index);
+		return;
 	}
+	sliderBullets[index].checked = false;
+	index++;
+	sliderBullets[index].checked = true;
 });
 buttonPrev.addEventListener("click", function () {
-	if (index > 0) {
-		delChecked(index);
-		index--;
-		addChecked(index);
-	} else {
-		delChecked(0);
-		index = sliderLength - 1;
-		addChecked(index);
+	if (index === 0) {
+		sliderBullets[0].checked = false;
+		sliderBullets[slider.length - 1].checked = true;
+		index = slider.length - 1;
+		return;
 	}
+	sliderBullets[index].checked = false;
+	index--;
+	sliderBullets[index].checked = true;
 });
 /* feedback modal window */
 const buttonFeedBack = document.querySelector(".button-contacts");
@@ -41,6 +34,7 @@ const closeFeedBack = feedBackWindow.querySelector(".close-feedback");
 const feedBackForm = feedBackWindow.querySelector(".feed-back-form");
 const feedBackNameInput = feedBackWindow.querySelector(".name-input");
 const feedBackEmailInput = feedBackWindow.querySelector(".email-input");
+const alert = feedBackWindow.querySelector(".alert");
 let isStorageSupport = true;
 let storage = "";
 try {
@@ -51,22 +45,35 @@ try {
 buttonFeedBack.addEventListener("click", function (evt) {
 	evt.preventDefault();
 	feedBackWindow.classList.add("active");
-	if(storage){
-		feedBackNameInput.value=storage;
+	if (storage) {
+		feedBackNameInput.value = storage;
+		feedBackEmailInput.focus();
+	} else {
+		feedBackNameInput.focus();
 	}
-	feedBackNameInput.focus();
 	body.classList.add("body-blocked");
 });
 closeFeedBack.addEventListener("click", function () {
 	feedBackWindow.classList.remove("active");
 	body.classList.remove("body-blocked");
 });
-feedBackForm.addEventListener("submit", function (evt) {
+ feedBackForm.addEventListener("submit", function (evt) {
 	if (!feedBackNameInput.value || !feedBackEmailInput.value) {
 		evt.preventDefault();
+		alert.classList.add("active-block");
 	} else {
+		alert.classList.remove("active-block");
 		if (isStorageSupport) {
 			localStorage.setItem("name", feedBackNameInput.value);
+		}
+	}
+});
+window.addEventListener("keydown",function(evt){
+	if(evt.code==="Escape"){
+		if(feedBackWindow.classList.contains("active")){
+			evt.preventDefault();
+			feedBackWindow.classList.remove("active");
+			body.classList.remove("body-blocked");
 		}
 	}
 });
